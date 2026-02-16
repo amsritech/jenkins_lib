19d1396296799b3f1029215312b0abadcce7ac13
srinivas <srinivas.bdn@gmail.com>
2026-02-17 00:26:32 +0530

    uploaded factorial

---
+++ python_scripts/python_programs/factorial.py
@@ -0,0 +1,16 @@
+n = 5
+fact = 1
+
+for i in range(1, n + 1):
+    fact *= i
+
+print(fact)
+
+
+##############recursion########################################
+
+def factorial(n):
+    if n ==0 or n ==1:
+        return 1
+    return n * factorial(n-1)
+print(factorial(5))
